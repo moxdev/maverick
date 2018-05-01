a1dca51d599cc995e949cf044e95143a9abcbb2c
moxdev <moxdev1@gmail.com>
2018-05-01 15:15:49 -0400

    working on gta

--- functions.php
+++ functions.php
@@ -42,11 +42,11 @@ if ( ! function_exists( 'maverick_setup' ) ) :
 		 */
     add_theme_support( 'post-thumbnails' );
     add_image_size('home-feature-img', 700, 700, true);
-    add_image_size('feature-img', 1800, 400, true);
+    add_image_size('feature-img', 1800, 600, true);
     add_image_size('featured-load-img', 600, 9999, false);
     add_image_size('team-bio-img', 300, 300, true);
     add_image_size('service-icons', 200, 87, true);
-    add_image_size('call-to-action-img', 1800, 300, true);
+    add_image_size('call-to-action-img', 1800, 400, true);
 
 		// This theme uses wp_nav_menu() in one location.
 		register_nav_menus( array(
